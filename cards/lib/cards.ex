7ac4415b2c1250c7a17e7b2d592a6073052d2a00
WranglerLab <jd.conradie@wranglerlab.com>
2023-12-22 09:05:11 +0200

    new project in the course

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,61 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for `Cards`.
+    Provides method for creating and handling cards interactions
   """
 
   @doc """
-  Hello world.
+      Returns list of strings representing playing cards
+    """
+  def create_deck do
+
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+
+
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  @doc """
+    Determines whether a deck contains a given card
 
   ## Examples
 
-      iex> Cards.hello()
-      :world
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
 
   """
-  def hello do
-    :world
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      # Using _ underscore when initiating a variable that will not be used, to avoid any compiler issues
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
